48363f10037ddeb30c25af8d6a644a646d4d94e3
ayabdi <46926470+ayabdi@users.noreply.github.com>
2020-11-20 11:33:39 +0000

    V1

--- config/db.js
+++ config/db.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
-
 const config = require('config');
+const db = 'mongodb://127.0.0.1:27017'
 
-const db = config.get('mongoURI');
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		});
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(db, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-        console.log('MongoDB Connected..')
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
+		console.log('MongoDB Connected...');
+	} catch (err) {
+		console.error(err.message);
+		// Exit process with failure
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,78 +1,76 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const { check, validationResult } = require("express-validator");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const normalize = require('normalize-url');
 
-//User model
-const User = require("../../models/User");
+const User = require('../../models/User');
 
-//@route POST api/users
-//@desc  Register User
-//@access Public
-
-//Validating entries
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 router.post(
-  "/",
+  '/',
   [
-    check("name", "Name is required").not().isEmpty(),
-    check("email", "Please include a valid email").isEmail(),
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
     check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 }),
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    //defining
+
     const { name, email, password } = req.body;
 
     try {
-      //see if user exists
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      //Get users gravatar
-      const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm",
-      });
+      const avatar = normalize(
+        gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm'
+        }),
+        { forceHttps: true }
+      );
 
-      // create user
       user = new User({
         name,
         email,
         avatar,
-        password,
+        password
       });
 
-      //Encrypt Password
       const salt = await bcrypt.genSalt(10);
+
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
 
-      //Return jsonwebtoken
-
       const payload = {
         user: {
-          id: user.id,
-        },
+          id: user.id
+        }
       };
 
       jwt.sign(
         payload,
-        config.get("jwtsecret"),
-        { expiresIn: 360000 },
+        config.get('jwtSecret'),
+        { expiresIn: '5 days' },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
@@ -80,7 +78,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send("Server error");
+      res.status(500).send('Server error');
     }
   }
 );
